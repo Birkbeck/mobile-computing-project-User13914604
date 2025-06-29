046bbcafe866ae3ad6f1ee4474e30a825afa6686
YourName <YourEmail@example.com>
2025-06-29 22:58:35 +0100

    Added test imp

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,4 +62,11 @@ dependencies {
     implementation("androidx.room:room-runtime:2.7.0")
     implementation("androidx.room:room-ktx:2.7.0")
     kapt("androidx.room:room-compiler:2.7.0")
+
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("androidx.arch.core:core-testing:2.2.0")
+    testImplementation("androidx.room:room-testing:2.7.0")
+    testImplementation("androidx.test:core:1.5.0")
+
+
 }
